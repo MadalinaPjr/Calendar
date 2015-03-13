d6d5bbfcf1bf11a9a11402db4f21b12cde62e7ee
Madalina <madalina.pjr@gmail.com>
2015-03-13 02:46:53 +0200

    IhaveNoIdeaWhatImDoing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,173 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Event;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('AppBundle::welcomeScreen.html.twig');
+    }
+
+    /**
+     * @Route("/monthly/{month}/{year}", name="monthly", defaults={"month" = 0, "year" = 0})
+     */
+    public function monthlyAction(Request $request, $month, $year)
+    {
+        $createEventForm = $this->createEventForm($request);
+
+        if ($month == 0 || $year == 0) {
+            $now = new \DateTime("now");
+            $month = $now->format("m");
+            $year = $now->format("Y");
+        } else {
+            $now = new \DateTime("1-" . $month . "-" . $year);
+        }
+
+        $calendarCells = $this->getCalendarCells($now);
+
+        $days = $calendarCells['noOfFullCells'];
+
+        $events = $this->getEvents($days, $now);
+
+        $weekdays = [
+            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+        ];
+
+        $prevDate =  date_create($now->format("d-m-Y") . " first day of last month");
+        $nextDate =  date_create($now->format("d-m-Y") . " first day of next month");
+
+        return $this->render('AppBundle::monthly.html.twig', [
+            'cells' => $calendarCells,
+            'weekdays' => $weekdays,
+            'events' => $events,
+            'monthName' => $this->getMonthName($month),
+            'year' => $year,
+            'prevDate' => $prevDate,
+            'nextDate' => $nextDate,
+            'form' => $createEventForm->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/event/{id}/edit", name="edit_event")
+     */
+    public function eventAction(Request $request, $id)
+    {
+        $event = $this->getDoctrine()->getRepository("AppBundle:Event")->find($id);
+
+        $form = $this->createEventForm($request, $event);
+
+        return $this->render('AppBundle::eventEdit.html.twig', [
+            'form' => $form->createView(),
+            'event' => $event
+        ]);
+    }
+
+    /**
+     * @param $now
+     * @return array
+     */
+    private function getCalendarCells($now)
+    {
+
+        $firstMonday = strtotime("first monday of " . $now->format("M Y"));
+        $firstMonday = new \DateTime(date("d-m-Y", $firstMonday));
+
+        $calendarCells['noOfEmptyCells'] = 7 - ($firstMonday->format("d") - 1);
+
+        $lastDayOfMonth = strtotime("last day of " . $now->format("M Y"));
+        $lastDayOfMonth = new \DateTime(date("d-m-Y", $lastDayOfMonth));
+
+        $calendarCells['noOfFullCells'] = $lastDayOfMonth->format("d");
+
+        $totalUsedDays = $calendarCells['noOfEmptyCells'] + (int)$calendarCells['noOfFullCells'];
+        if ($totalUsedDays <= 28) {
+            $totalNoOfCalendarCells = 28;
+        } elseif ($totalUsedDays <= 35) {
+            $totalNoOfCalendarCells = 35;
+        } else {
+            $totalNoOfCalendarCells = 42;
+        }
+
+        $calendarCells['noOfLastEmptyCells'] = $totalNoOfCalendarCells - ((int)$calendarCells['noOfFullCells'] + $calendarCells['noOfEmptyCells']);
+
+        return $calendarCells;
+    }
+
+    private function getEvents($days, $now)
+    {
+        $events = [];
+        // TODO: Find a way to get all events for this month in a single query using the MySql IN condition
+        for ($i = 1; $i <= $days; $i++) {
+            $day = new \DateTime($i . "-" . $now->format("m-Y"));
+            $events[$i] = $this->getDoctrine()->getRepository("AppBundle:Event")->findBy(['date' => $day]);
+        }
+
+        return $events;
+    }
+
+    private function getMonthName($month)
+    {
+        $months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+        $monthName = $months[$month - 1];
+
+        return $monthName;
+    }
+
+    private function createEventForm($request, $event = null)
+    {
+        if ($event == null) {
+            $event = new Event();
+        }
+
+        $createEventForm = $this->createFormBuilder($event);
+        $createEventForm->add('title');
+        $createEventForm->add('description');
+        $createEventForm->add('date');
+        $createEventForm->add('priority', 'choice', [
+            'choices' => [
+                'active' => 'active',
+                'success' => 'success',
+                'info' => 'info',
+                'warning' => 'warning',
+                'danger' => 'danger'
+            ]
+        ]);
+        $createEventForm = $createEventForm->getForm();
+
+        $createEventForm->handleRequest($request);
+        if ($createEventForm->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($event);
+            $manager->flush();
+
+            return new RedirectResponse($this->generateUrl('monthly'));
+        }
+
+        return $createEventForm;
+    }
+
+    /**
+     * @Route("/event/{id}/delete", name="delete_event")
+     */
+    public function deleteEvent($id)
+    {
+        $event = $this->getDoctrine()->getRepository("AppBundle:Event")->find($id);
+
+        $this->getDoctrine()->getManager()->remove($event);
+        $this->getDoctrine()->getManager()->flush();
+
+        return new RedirectResponse($this->generateUrl('monthly'));
     }
 }
---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="event")
+ * @ORM\Entity
+ */
+class Event
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string")
+     */
+    protected $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    protected $description;
+
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    protected $date;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="priority", type="string")
+     */
+    protected $priority;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param string $priority
+     */
+    public function setPriority($priority)
+    {
+        $this->priority = $priority;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
